28c48676cad6b92262b243ef0e1a1c0ac40f37ca
Tim Mauersberger <tim-mauersberger@gmx.de>
2025-02-26 15:16:20 +0100

    Added Economy to the helper module

--- src/main/java/de/tmxx/trading/module/helper/HelperModule.java
+++ src/main/java/de/tmxx/trading/module/helper/HelperModule.java
@@ -1,8 +1,16 @@
 package de.tmxx.trading.module.helper;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import de.tmxx.trading.helper.ListenerHelper;
 import de.tmxx.trading.helper.impl.ListenerHelperImpl;
+import de.tmxx.trading.module.plugin.PluginLogger;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+import java.util.logging.Logger;
 
 /**
  * Project: trading
@@ -15,5 +23,17 @@ public class HelperModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(ListenerHelper.class).to(ListenerHelperImpl.class);
+    }
+
+    @Provides
+    @Singleton
+    Economy provideEconomy(@PluginLogger Logger logger) {
+        RegisteredServiceProvider<Economy> economyProvider = Bukkit.getServicesManager().getRegistration(Economy.class);
+        if (economyProvider == null) {
+            logger.severe("Could not find Vault economy service provider");
+            return null;
+        }
+
+        return economyProvider.getProvider();
     }
 }
